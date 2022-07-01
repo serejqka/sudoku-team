971c4b304a9a13275e887f528d1ab1194cd7131e
Илья Маркин <kami_zer0@MacBook-Pro-Ila.local>
2022-07-01 14:41:19 +0300

    check1

--- sudoku.js
+++ sudoku.js
@@ -1,16 +1,74 @@
-/**
- * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
- * Возвращает игровое поле после попытки его решить.
- * Договорись со своей командой, в каком формате возвращать этот результат.
- */
+
+
+//  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
+//  * Возвращает игровое поле после попытки его решить.
+//  * Договорись со своей командой, в каком формате возвращать этот результат.
+//  */
 function solve(boardString) {
+  let string = '', bag = [];
+
+  for (i = 0; i< boardString.length; i++){
+    string = string + boardString[i]
+    if (string.length % 9 === 0){
+      bag.push([...string])
+      string = ''
+    }
+  }
+
+  if(isSolved(bag)){
+    return true
+  }
+
+  return false
+
 }
 
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает булевое значение — решено это игровое поле или нет.
- */
+// /
+//  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
+//  * Возвращает булевое значение — решено это игровое поле или нет.
+//  */
+
+//Работаем с числом
+function workSpace(board){
+  
+
+  for (let t = 0; t < bag[i]; t++){
+
+    if(a === bag[i][t]){
+      return false
+    }
+
+    if(a === bag[t][j]){
+      return false
+    }
+
+    if()
+
+  }
+    bag[i][j])
+  if (a === )
+}
+
+// Наша функция поиска -
+function findSpace(board){
+  for (let a = 1; a < 10; a++){
+    if (workSpace(a)) continue
+
+  }
+}
+
 function isSolved(board) {
+
+  console.log(bag);
+  findWord(bag)
+  for (let i = 0; i < bag.length; i++) {
+    for (let j = 0; j < bag[i].length; j++) {
+      if (bag[i][j] === "-") {
+        return bag[i][j];
+      }
+      return null;
+    }
+  }
 
 }
 
@@ -29,3 +87,6 @@ module.exports = {
   isSolved,
   prettyBoard,
 };
+
+
+
